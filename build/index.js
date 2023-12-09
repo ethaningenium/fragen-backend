ca0375cb1753d7021a4cf15e4ca09775f2e8f1e6
ethaningenium <143124094+ethaningenium@users.noreply.github.com>
2023-12-09 22:19:49 +0100

    deployed

--- build/index.js
+++ build/index.js
@@ -1,14 +1,49 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const userControl = __importStar(require("./controllers/UserController"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const port = 8000;
-app.get('/', (req, res) => {
-    res.send('22222');
+mongoose_1.default
+    .connect('mongodb+srv://xvii1103:T4Ptjpge6zcRdbax@train.h5aiaqn.mongodb.net/quizapp?retryWrites=true&w=majority')
+    .then(() => {
+    console.log('mongodb works');
+})
+    .catch(() => {
+    console.log('catch');
 });
+app.post('/', (req, res) => {
+    res.send('ich bin pidor');
+});
+app.post('/register', userControl.register);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
