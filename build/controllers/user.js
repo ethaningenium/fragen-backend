0c89eb1e2d6ca31ff279d90503cdbdbcf01bc301
ethaningenium <143124094+ethaningenium@users.noreply.github.com>
2023-12-17 17:46:39 +0100

    Delete build directory

--- build/controllers/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getrandom = void 0;
-const getrandom = () => {
-    return Math.floor(Math.random() * 100) + 1;
-};
-exports.getrandom = getrandom;
-//# sourceMappingURL=user.js.map
